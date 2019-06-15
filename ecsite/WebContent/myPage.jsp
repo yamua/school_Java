44da19b8db9ad8e87659d3e364a54040b9aaf9ce
yamua <>
2019-06-15 20:38:21 +0900

    myPage.jspの<s:if test="myPageList == null">が効かない。 MyPageActionでDBから取得した履歴情報を「myPageList」に格納する処理「myPageList = myPageDAO.getMyPageUserInfo(item_transaction_id, user_master_id);」が出来ていないのかも？？ 購入情報はDBには入っているので、そこから呼び出せていなだけだと思う。
    ちなみに、新規ユーザー登録できるようになりました。書きミスでした。

--- ecsite/WebContent/myPage.jsp
+++ ecsite/WebContent/myPage.jsp
@@ -8,7 +8,8 @@
 	<meta http-equiv="Content-Style-Type" content="text/css" />
 	<meta http-equiv="Content-Script-Type" content="text/javascript" />
 	<meta http-equiv="imagetoolbar" content="no" />
-	<meta name="description" content="" /> <meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
 	<title>MyPage画面</title>
 	<style type="text/css">
 		/*=== TAG LAYOUT ===*/
@@ -64,14 +65,14 @@
 			<p>MyPage</p>
 		</div>
 		<div>
-			<s:if test="myPageList ==null">
+			<s:if test="myPageList == null">
 				<h3>ご購入情報はありません。</h3>
 			</s:if>
 			<s:elseif test="message == null">
 				<h3>ご購入情報は以下になります。</h3>
 				<table border="1">
 					<tr>
-						<th>商品名</th>
+						<th>jjjjjjj商品名</th>
 						<th>値段</th>
 						<th>購入個数</th>
 						<th>支払い方法</th>
@@ -86,6 +87,7 @@
 							<td><s:property value="insert_date"/></td>
 						</tr>
 					</s:iterator>
+
 				</table>
 				<s:form action="MyPageAction">
 					<input type="hidden" name="deleteFlg" value="1">
--- ecsite/src/com/internousdev/ecsite/dao/MyPageDAO.java
+++ ecsite/src/com/internousdev/ecsite/dao/MyPageDAO.java
@@ -16,13 +16,7 @@ public class MyPageDAO {
 	//DBから購入履歴を取得するためのメソッド
 	public ArrayList<MyPageDTO> getMyPageUserInfo(String item_transaction_id, String user_master_id) throws SQLException {
 		ArrayList<MyPageDTO> myPageDTO = new ArrayList<MyPageDTO>();
-		String sql =
-				"SELECT ubit.id, iit.item_name, ubit.total_price, ubit.total_count, ubit.pay, ubit.insert_date"
-				+ "FROM user_buy_item_transaction ubit"
-				+ "LEFT JOIN item_info_transaction iit" //「LEFT JOIN」を用いて複数のテーブルを結合することによりユーザ情報と履歴情報を紐付けて一括取得できる
-				+ "ON ubit.item_transaction_id = iit.id"
-				+ "WHERE ubit.item_transaction_id = ? AND ubit.user_master_id = ?"
-				+ "ORDER BY insert_date DESC";
+		String sql = "SELECT ubit.id, iit.item_name, ubit.total_price, ubit.total_count, ubit.pay, ubit.insert_date FROM user_buy_item_transaction ubit LEFT JOIN item_info_transaction iit ON ubit.item_transaction_id = iit.id WHERE ubit.item_transaction_id = ? AND ubit.user_master_id = ? ORDER BY insert_date DESC";
 
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
